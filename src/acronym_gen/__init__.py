07bd0cb82634309d8d4e941162b6b453cdbe293d
Zain Aamer <ZippeyKeys12@gmail.com>
2019-11-11 22:24:07 -0600

    Acronym changes

--- src/acronym_gen/__init__.py
+++ src/acronym_gen/__init__.py
@@ -2,7 +2,7 @@ import math
 import re
 from typing import Dict, List
 
-import requests
+from datamuse import Datamuse
 from reportlab.lib.enums import TA_CENTER
 from reportlab.lib.pagesizes import letter
 from reportlab.lib.styles import ParagraphStyle, getSampleStyleSheet
@@ -10,7 +10,7 @@ from reportlab.platypus import Paragraph, SimpleDocTemplate, Spacer
 from tracery import Grammar
 from tracery.modifiers import base_english
 
-from ..abstracts import Generator
+from ..abstracts import Generator, Processor
 
 
 class AcronymGenerator(Generator):
@@ -19,6 +19,8 @@ class AcronymGenerator(Generator):
     def __init__(self, acronym: str):
         self.acronym = acronym
 
+        api = Datamuse()
+
         dictionary: Dict[str, List[str]] = {}
 
         splitted = self.splitting_pattern.findall(acronym)
@@ -26,23 +28,33 @@ class AcronymGenerator(Generator):
         self.length = len(splitted)
 
         for start in splitted:
-            r = requests.get('https://api.datamuse.com/words', params={
-                'sp': '{}*'.format(start)
-            })
+            res = api.words(sp='{}*'.format(start))
 
-            dictionary[start] = [obj['word'] for obj in r.json()]
+            dictionary[start] = [obj['word'] for obj in res]
 
         self.grammar = Grammar(dictionary)
         self.grammar.add_modifiers(base_english)
 
         self.rule = '#{}.capitalize#'.format('.capitalize# #'.join(splitted))
 
-    def generate_text(self, length: int = 50000) -> str:
+    def generate_text(self, **kwargs) -> str:
+        try:
+            length = kwargs['length']
+        except KeyError:
+            length = None
+        length = length or 50000
+
         return '\n'.join((self.grammar.flatten(self.rule)
                           for _ in range(math.ceil(length / self.length))))
 
-    def save_to_file(self, file_name: str, length: int = 50000):
-        text = self.generate_text(length)
+    def save_to_file(self, file_name: str, **kwargs):
+        try:
+            length = kwargs['length']
+        except KeyError:
+            length = None
+        length = length or 50000
+
+        text = self.generate_text(length=length)
 
         if file_name.endswith('.pdf'):
             doc = SimpleDocTemplate(file_name, pagesize=letter,
@@ -63,6 +75,31 @@ class AcronymGenerator(Generator):
         else:
             with open(file_name, 'w') as f:
                 f.write(text)
+
+
+class AcronymProcessor(Processor):
+    splitting_pattern = re.compile('[A-Z][^A-Z]*')
+
+    def __init__(self):
+        self.api = Datamuse()
+
+        self.grammar = Grammar({})
+        self.grammar.add_modifiers(base_english)
+
+    def process_text(self, input_text: str, **kwargs) -> str:
+        splitted = self.splitting_pattern.findall(input_text)
+
+        dictionary: Dict[str, List[str]] = {}
+        for start in (x for x in splitted if x not in self.grammar.symbols):
+            res = self.api.words(sp='{}*'.format(start), max=1000)
+
+            dictionary[start] = [obj['word'] for obj in res]
+
+        for k, v in dictionary.items():
+            self.grammar.push_rules(k, v)
+
+        return self.grammar.flatten('#{}.capitalize#'.format(
+            '.capitalize# #'.join(splitted)))
 
 
 def main():
